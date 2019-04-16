0f9543e006e990c994d9bd0eae17dd7f3d5f9ad9
Phillip <phillip@Phillips-MacBook-Pro.local>
2019-04-16 15:47:45 +0200

    Updated to new workflow

--- git/gitclean.sh
+++ git/gitclean.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 function gitclean () {
-    if [ "$(git branch | grep  -v "master" | grep -v "develop")" != "" ]
+    if [ "$(git branch | grep  -v "master" | grep -v "release")" != "" ]
     then
-        git branch | grep -v "master" | grep -v "develop" | xargs git branch -d
+        git branch | grep -v "master" | grep -v "release" | xargs git branch -d
     fi
 
     return 0
