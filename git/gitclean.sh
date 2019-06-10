e17715bb66aa575d83813717d6a24bd1374f563d
fishyfishphil <phillipdebruin1@gmail.com>
2019-06-10 17:56:32 +0200

    Update git commands

--- git/gitclean.sh
+++ git/gitclean.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 function gitclean () {
-    if [ "$(git branch | grep  -v "master" | grep -v "release")" != "" ]
+    LIST=$(git branch)
+    echo $LIST
+    if [ "$(git branch | grep  -v "*" | grep  -v "master" | grep -v "release")" != "" ]
     then
-        git branch | grep -v "master" | grep -v "release" | xargs git branch -d
+        git branch | grep -v "*" | grep -v "master" | grep -v "release" | xargs git branch -d
     fi
 
     return 0
