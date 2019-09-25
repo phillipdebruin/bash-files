3196dd3b880093072968fc27d13b09a2859ebee8
fishyfishphil <phillipdebruin1@gmail.com>
2019-09-25 11:52:11 +0200

    Improve prune and ps commands

--- docker/aliases.sh
+++ docker/aliases.sh
@@ -2,6 +2,6 @@
 
 alias dockup="docker-compose up"
 alias dockupb="docker-compose up --build"
-alias dockps="docker ps"
-alias dockpsa="docker ps -a"
-alias dockprune="docker images -f dangling=true | awk '{print $3}' | xargs docker rmi"
+alias dockps="docker ps --format 'table {{.Names}}\t{{.Ports}}\t{{.Networks}}\t{{.Image}}\t{{.Status}}\t{{.RunningFor}}\t{{.Size}}\t{{.Command}}'"
+alias dockpsa="docker ps -a --format 'table {{.Names}}\t{{.Ports}}\t{{.Networks}}\t{{.Image}}\t{{.Status}}\t{{.RunningFor}}\t{{.Size}}\t{{.Command}}'"
+alias dockprune="docker rmi $(docker images -f "dangling=true" -q)"
